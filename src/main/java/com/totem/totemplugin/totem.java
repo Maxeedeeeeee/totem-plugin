3af386bbaa4a696311afc51adb2de26b6c09e70f
SharpyGH <110199236+Maxeedeeeeee@users.noreply.github.com>
2024-08-28 14:11:54 -0500

    Update totem.java

--- src/main/java/com/totem/totemplugin/totem.java
+++ src/main/java/com/totem/totemplugin/totem.java
@@ -1,1 +1,38 @@
+package com.totem.mytotemplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyTotemPlugin extends JavaPlugin implements Listener {
+
+    @Override  
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("MyTotemPlugin has been enabled!");
+    }
+
+    @Override  
+    public void onDisable() {
+        getLogger().info("MyTotemPlugin has been disabled.");
+    }
+
+    @EventHandler  
+    public void onRightClickTotem(PlayerInteractEvent event) {
+        if (event.getAction().toString().contains("RIGHT_CLICK") && 
+            event.getItem() != null && 
+            event.getItem().getType() == Material.TOTEM_OF_UNDYING) {
+
+            ItemStack totem = event.getItem();
+            if (event.getPlayer().getInventory().getItemInOffHand().getType() == Material.AIR) {
+                event.getPlayer().getInventory().setItemInOffHand(totem);
+                event.getPlayer().getInventory().remove(totem);
+                event.getPlayer().sendMessage("You have moved the Totem of Undying to your offhand!");
+            }
+        }
+    }
+}
